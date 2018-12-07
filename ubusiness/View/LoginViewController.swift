047d510da52b848fcec16944fc58ee527d6ea45b
Konstantin Kulakov <i@kostyakulakov.ru>
2018-12-08 01:23:12 +0300

    Move to MVVM pattern

---
+++ ubusiness/View/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  ubusiness
+//
+//  Created by Константин Кулаков on 07.12.2018.
+//  Copyright © 2018 Константин Кулаков. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var phoneField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
